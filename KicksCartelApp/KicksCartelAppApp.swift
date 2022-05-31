dab6d1bca39550050216139adf82b63519744370
lrbl <luis.lemini.98@gmail.com>
2022-05-30 22:50:35 -0500

    LaunchScreen in Plist

--- KicksCartelApp/KicksCartelAppApp.swift
+++ KicksCartelApp/KicksCartelAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KicksCartelAppApp: App {
     var body: some Scene {
         WindowGroup {
-            BrowseView()
+            LaunchScreen()
         }
     }
 }
---
+++ KicksCartelApp/LaunchScreen.swift
@@ -0,0 +1,36 @@
+//
+//  LaunchScreen.swift
+//  KicksCartelApp
+//
+//  Created by Luis Roberto Blancas Lemini on 28/05/22.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    @State private var isReadyToNavigate = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                NavigationLink(destination: BrowseView(),
+                               isActive: $isReadyToNavigate) { EmptyView() }
+                Color.black
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .ignoresSafeArea()
+            .task {
+                try? await Task.sleep(nanoseconds: 2_500_000_000)
+                isReadyToNavigate = true
+        }
+        }
+    }
+}
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
