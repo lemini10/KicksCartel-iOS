24c192493a52f03c7884ceedadeaac76760369ad
lrbl <luis.lemini.98@gmail.com>
2022-07-17 01:25:10 -0500

    Fixed bug

--- KicksCartelApp/Auxiliary Views/CheckoutViewModel.swift
+++ KicksCartelApp/Auxiliary Views/CheckoutViewModel.swift
@@ -12,26 +12,14 @@ class CheckoutViewModel: ObservableObject {
     
     var items: [FetchedSneaker]
     var model: CardModel = CardModel(type: .Credit, name: "Luis Lemini", maskedNumbers: "**** **** **** 0000")
-    var cancellables: Set<AnyCancellable> = Set()
     
     init(items: [FetchedSneaker]) {
         self.items = items
     }
     
     func purchaseItems() {
-        RemoteDataManager.shared.purchaseItems {
+        RemoteDataManager.shared.purchaseItems() {
             print("Successfully purchased")
-        }.sink { completion in
-            switch completion {
-            case .finished:
-                print("Fetched favorites successfully")
-            case .failure(let error):
-                print(error)
-            }
-        } receiveValue: { fetchedSneakersArray in
-            self.fetchedSneakers = fetchedSneakersArray
         }
-        .store(in: &cancellables)
     }
-    
 }
